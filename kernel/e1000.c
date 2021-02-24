b606cae47aee14c5c8238dd745057673b5affeb8
Xiao Fan <tommyfanzju@gmail.com>
2021-02-24 21:04:34 +0800

    lab net: finished

--- kernel/e1000.c
+++ kernel/e1000.c
@@ -102,7 +102,24 @@ e1000_transmit(struct mbuf *m)
   // the TX descriptor ring so that the e1000 sends it. Stash
   // a pointer so that it can be freed after sending.
   //
-  
+  acquire(&e1000_lock);
+  uint32 tx_index = regs[E1000_TDT];
+  if ((tx_ring[tx_index].status & E1000_TXD_STAT_DD) == 0) {
+    release(&e1000_lock);
+    return -1;
+  } else if (tx_mbufs[tx_index] != 0) {
+    mbuffree(tx_mbufs[tx_index]);
+  }
+  tx_mbufs[tx_index] = m;
+  tx_ring[tx_index].addr = (uint64)m->head;
+  tx_ring[tx_index].length = m->len;
+  tx_ring[tx_index].cmd = E1000_TXD_CMD_EOP | E1000_TXD_CMD_RS;
+  regs[E1000_TDT] = (tx_index + 1) % TX_RING_SIZE;
+  if (&tx_ring[tx_index] == 0 || tx_mbufs[tx_index] == 0) {
+    release(&e1000_lock);
+    return -1;
+  }
+  release(&e1000_lock);
   return 0;
 }
 
@@ -115,6 +132,18 @@ e1000_recv(void)
   // Check for packets that have arrived from the e1000
   // Create and deliver an mbuf for each packet (using net_rx()).
   //
+  while (1) {
+    uint32 rx_index = (regs[E1000_RDT]+1)%RX_RING_SIZE;
+    if ((rx_ring[rx_index].status & E1000_RXD_STAT_DD) == 0) {
+      break;
+    }
+    rx_mbufs[rx_index]->len = rx_ring[rx_index].length;
+    net_rx(rx_mbufs[rx_index]);
+    rx_mbufs[rx_index] = mbufalloc(0);
+    rx_ring[rx_index].addr = (uint64)rx_mbufs[rx_index]->head;
+    rx_ring[rx_index].status = 0;
+    regs[E1000_RDT] = rx_index;
+  }
 }
 
 void
--- user/nettests.c
+++ user/nettests.c
@@ -198,7 +198,7 @@ dns()
   memset(ibuf, 0, N);
   
   // 8.8.8.8: google's name server
-  dst = (8 << 24) | (8 << 16) | (8 << 8) | (8 << 0);
+  dst = (114 << 24) | (114 << 16) | (114 << 8) | (114 << 0);
 
   if((fd = connect(dst, 10000, 53)) < 0){
     fprintf(2, "ping: connect() failed\n");
